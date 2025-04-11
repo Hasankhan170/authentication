560e6d25827dd8bdf8615a7ef2d856672f50a7ee
Hasan Khan <hasankhanali0316@gmail.com>
2025-04-11 14:15:19 +0500

    done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,62 +1,61 @@
-'use client'
-import axios from 'axios';
-import { log } from 'console';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
+"use client";
+import axios from "axios";
+import { log } from "console";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 type User = {
-    _id: string;
-    name: string;
-    email?: string; 
-  };
-  
+  _id: string;
+  name: string;
+  email?: string;
+};
 
 const page = () => {
-    const router = useRouter();
-    const [data, setData] = useState<User | null>(null)
+  const router = useRouter();
+  const [data, setData] = useState<User | null>(null);
 
-    const getDataFromToken = async () =>{
-        try {
-            const res = await axios.post('/api/users/me')
-            console.log(res.data);
-            setData(res.data.data)
-            
-            } catch (error) {
-            console.log(error);
-            
-        }
+  const getDataFromToken = async () => {
+    try {
+      const res = await axios.post("/api/users/me");
+      console.log(res.data);
+      setData(res.data.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getDataFromToken();
-      }, []);
-      
+  useEffect(() => {
+    getDataFromToken();
+  }, []);
 
-    const logout = async ()=>{
-        try {
-            const res = await axios.get('/api/users/logout')
-            toast.success("User logged out successfully")
-            router.push('/login')
-        } catch (error:any) {
-            console.log(error);
-            toast.error(error.message)
-            
-        }
+  const logout = async () => {
+    try {
+      const res = await axios.get("/api/users/logout");
+      toast.success("User logged out successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
   return (
     <div>
-        <h1>Profile</h1>
-        <h2>
-        {!data
-          ? 'Loading...'
-          : <Link href={`/profile/${data._id}`}>{data.name} <br /> {data.email}</Link>}
-      </h2>
-        <hr />
-        <button onClick={logout}>Logout</button>
-    </div>
-  )
-}
+      <h1>Profile</h1>
 
-export default page
+      {!data ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          <h2>{data.name}</h2>
+          <p>{data.email}</p>
+        </div>
+      )}
+      <hr />
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
+
+export default page;
